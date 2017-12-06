dfb875f6bfa332eded96835d779fa11bdba107ae
julyL <849424162@qq.com>
2017-12-06 19:05:03 +0800

    write outline

--- dist/Store.js
+++ dist/Store.js
@@ -1,29 +1,35 @@
-const lodashGet = require('lodash.get');
-const lodashSet = require('lodash.Set');
-var noop = function () {},
-    isSupported = _isStorageSupported(localStorage),
-    defaultOptions = {
-        Storage: "localStorage",
-        exp: 315360000000, // 默认超时10000年
-        serialize(data) {
-            return JSON.stringify(data);
-        },
-        deserialize(data) {
-            return data && JSON.parse(data);
-        },
-        polyfill: { // 不支持localStorage时,可以通过实现以下函数来进行polyfill
-            getItem: noop,
-            setItem: noop,
-            removeItem: noop,
-            getAllStorage: noop
-        }
-    }
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.Store = factory());
+}(this, (function () { 'use strict';
 
-var Store = function (opts) {
+var lodashGet = require('lodash.get');
+var lodashSet = require('lodash.Set');
+var noop = function noop() {};
+var isSupported = _isStorageSupported(localStorage);
+var defaultOptions = {
+    Storage: "localStorage",
+    exp: 315360000000, // 默认超时10000年
+    serialize: function serialize(data) {
+        return JSON.stringify(data);
+    },
+    deserialize: function deserialize(data) {
+        return data && JSON.parse(data);
+    },
+
+    polyfill: { // 不支持localStorage时,可以通过实现以下函数来进行polyfill
+        getItem: noop,
+        setItem: noop,
+        removeItem: noop,
+        getAllStorage: noop
+    }
+};
+
+var Store = function Store(opts) {
     var that = this;
     if (!(this instanceof Store)) {
-        throw new TypeError("Failed to construct 'Store': Please use the 'new' operator, this object construc" +
-                "tor cannot be called as a function.");
+        throw new TypeError("Failed to construct 'Store': Please use the 'new' operator, this object construc" + "tor cannot be called as a function.");
     }
     this.opts = _extend(defaultOptions, opts);
 
@@ -36,7 +42,9 @@ var Store = function (opts) {
         var polyFn = that.opts.polyfill[funcName];
         if (polyFn === noop) {
             if (funcName == "getAllStorage") {
-                return () => window[that.opts.Storage];
+                return function () {
+                    return window[that.opts.Storage];
+                };
             } else {
                 return window[that.opts.Storage][funcName];
             }
@@ -44,19 +52,19 @@ var Store = function (opts) {
             //如果要在不支持localStorage的环境中使用,可自行实现opts.polyfill[funcName]
             return polyFn;
         } else {
-            throw new TypeError(`Polyfill ${funcName} should be a function`);
+            throw new TypeError('Polyfill ' + funcName + ' should be a function');
         }
     }
-}
+};
 
 function isFunction() {
     return typeof fn === 'function';
 }
 
 function _extend(obj, props) {
-    for (var key in props) 
+    for (var key in props) {
         obj[key] = props[key];
-    return obj;
+    }return obj;
 }
 
 function _isStorageSupported(storage) {
@@ -84,21 +92,20 @@ function isLegalStruct(data) {
 
 Store.prototype = {
     constructor: Store,
-    get(key) {
+    get: function get(key) {
         var storageData = this.getAllStorage(),
             firstKey = key.split(".")[0],
-            parsedData = this
-                .opts
-                .deserialize(storageData[firstKey]);
+            parsedData = this.opts.deserialize(storageData[firstKey]);
         if (isLegalStruct(parsedData)) {
-            if (+ new Date() >= parsedData.__end__) { // 已过期
+            if (+new Date() >= parsedData.__end__) {
+                // 已过期
                 this.remove(firstKey);
             } else {
                 return lodashGet(parsedData.__data__, key);
             }
         }
     },
-    set(key, val, opts) {
+    set: function set(key, val, opts) {
         var opts = _extend(this.opts, opts),
             storageData = this.getAllStorage(),
             firstKey = key.split(".")[0],
@@ -108,13 +115,14 @@ Store.prototype = {
             __start__: nowTimeStamp,
             __end__: expiresTime,
             __data__: lodashSet(this.opts.deserialize, key, val)
-        }))
+        }));
     },
-    remove(key) {
+    remove: function remove(key) {
         this.removeItem(key);
     },
-    isSupported,
-    clearAllExpires() {
+
+    isSupported: isSupported,
+    clearAllExpires: function clearAllExpires() {
         var storageData = this.getAllStorage(),
             nowTimeStamp = +new Date();
         for (var key in storageData) {
@@ -125,5 +133,8 @@ Store.prototype = {
             }
         }
     }
-}
-export default Store;
+};
+
+return Store;
+
+})));
---
+++ index.js
@@ -0,0 +1,41 @@
+import countdown from "./src/countdown";
+import formatDate from "./src/formatDate";
+import getDays from "./src/getDays";
+import getDaysBetween from "./src/getDaysBetween";
+import isLeapYear from "./src/isLeapYear";
+import parseDate from "./src/parseDate";
+import addClass from "./src/addClass";
+import removeClass from "./src/removeClass";
+import hasClass from "./src/hasClass";
+import offset from "./src/offset";
+import scrollTo from "./src/scrollTo";
+import debounce from "./src/debounce";
+import leftpad from "./src/leftpad";
+import throttle from "./src/throttle";
+import Url from "./src/Url";
+import safeGet from "./src/safeGet";
+import safeSet from "./src/safeSet";
+import asyncLoadJs from "./src/asyncLoadJs";
+import Base64 from "./src/Base64";
+
+export {
+  countdown,
+  formatDate,
+  getDays,
+  getDaysBetween,
+  isLeapYear,
+  parseDate,
+  addClass,
+  removeClass,
+  hasClass,
+  offset,
+  scrollTo,
+  debounce,
+  leftpad,
+  throttle,
+  Url,
+  safeGet,
+  safeSet,
+  asyncLoadJs,
+  Base64
+};
